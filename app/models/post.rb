bde3ecbf0d8be871c7357812b0b0991c88c1fc39
Satoshi170 <Satoshi170@example.com>
2023-03-06 16:48:13 +0900

    各データのバリデーションを実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,32 @@
 class Post < ApplicationRecord
+    validates :title,
+        presence: true,
+        length: { maximum: 20}
+    
+    validates :startdate,
+        presence: true
+    
+    validate :start_check    
+    
+    validates :enddate,
+        presence: true
+    
+    validate :start_end_check
+
+    validates :content,
+        length: { maximum: 500}
+    
     attribute :allday, :boolean, default: false
+
+    def start_check
+        errors.add(:startdate, "は過去の日付で登録できません。") 
+        unless self.startdate >= Date.current
+        end
+    end
+
+    def start_end_check
+        errors.add(:enddate, "は開始日より前の日付は登録できません。") 
+        unless self.enddate >= self.startdate
+        end
+    end
 end
