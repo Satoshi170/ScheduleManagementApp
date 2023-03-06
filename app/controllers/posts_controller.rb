3c5036889ea93a1ec76a59b288217cce8928a9e4
Satoshi170 <Satoshi170@example.com>
2023-03-06 23:30:11 +0900

    homeのviewを大体完成させた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,34 @@
 class PostsController < ApplicationController
+  #スケジュールの一覧表示
   def home
     @posts = Post.all
   end
 
+  #スケジュールの作成
+  def new
+  end
+
+  #スケジュールの登録
   def create
   end
+
+  #スケジュールの詳細ページ
+  def show
+    @post = Post.find_by(params[:id])
+  end
+
+  #スケジュールの編集
+  def edit
+    @post = Post.find_by(params[:id])
+  end
+
+  def update
+    
+  end
+
+  #スケジュールの削除
+  def destroy
+    @post = Post.find_by(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root 'posts#home'
-  get 'posts/home'
-  get 'posts/create'
+  get "/" => "posts#home"
+  get "home" => "posts#home"
+  get "new" => "posts#new"
+  get "create" => "posts#create"
+  get ":id/show" => "posts#show"
+  get ":id/edit" => "posts#edit"
+  get ":id/destroy" => "posts#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
